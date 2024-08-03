bd4fbac72f8e03285c9bd1c40aca32e5b8906a75
mike-box <2876768308@qq.com>
2024-08-03 12:40:09 +0800

    add by mike meng

--- exercises/14_class_move/main.cpp
+++ exercises/14_class_move/main.cpp
@@ -6,7 +6,7 @@
 
 class DynFibonacci {
     size_t *cache;
-    int cached;
+    mutable int cached;
 
 public:
     // TODO: 实现动态设置容量的构造器
@@ -16,10 +16,8 @@ public:
     }
 
     // TODO: 实现移动构造器
-    DynFibonacci(DynFibonacci &&other) {
-        delete cache;
-        cache = other.cache;
-        cached = other.cached;
+    DynFibonacci(DynFibonacci &&other) noexcept 
+        : cache(other.cache), cached(other.cached) {
         other.cache = nullptr;
         other.cached = 0;
     }
@@ -28,22 +26,23 @@ public:
     // NOTICE: ⚠ 注意移动到自身问题 ⚠
     DynFibonacci &operator=(DynFibonacci &&other) {
         if (this != &other) { 
-            delete cache;
+            delete[] cache;
             cache = other.cache;
             cached = other.cached;
             other.cache = nullptr;
             other.cached = 0;
         }
+        return *this;
     }
 
     // TODO: 实现析构器，释放缓存空间
     ~DynFibonacci() {
-        delete cache;
+        delete[] cache;
         cache = nullptr;
     }
 
     // TODO: 实现正确的缓存优化斐波那契计算
-    size_t operator[](int i) {
+    size_t operator[](int i) const {
         for (; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
